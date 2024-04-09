2984de842e925fd6f698d74e46279ec07d9d9ccc
lucas-manuel <lucasmanuel.tech@gmail.com>
2024-04-09 08:23:38 -0400

    feat: add addresses

---
+++ src/Base.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity ^0.8.13;
+
+library Base {
+
+    /******************************************************************************************************************/
+    /*** DSR Oracle Addresses                                                                                       ***/
+    /******************************************************************************************************************/
+
+    address internal constant DSR_AUTH_ORACLE            = 0x2Dd2a2Fe346B5704380EfbF6Bd522042eC3E8FAe;
+    address internal constant DSR_RECEIVER               = 0xaDEAf02Ddb5Bed574045050B8096307bE66E0676;
+    address internal constant DSR_BALANCER_RATE_PROVIDER = 0xeC0C14Ea7fF20F104496d960FDEBF5a0a0cC14D0;
+
+}
