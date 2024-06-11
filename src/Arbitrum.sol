60d5b467d418eb0f90892299fcb66789579a7709
Bartek Rutkowski <barrutko.eth@gmail.com>
2024-06-11 20:09:35 +0200

    feat: add Arbitrum addresses

---
+++ src/Arbitrum.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity ^0.8.13;
+
+library Arbitrum {
+
+    /******************************************************************************************************************/
+    /*** DSR Oracle Addresses                                                                                       ***/
+    /******************************************************************************************************************/
+
+    address internal constant DSR_AUTH_ORACLE            = 0xE206AEbca7B28e3E8d6787df00B010D4a77c32F3;
+    address internal constant DSR_RECEIVER               = 0xcA61540eC2AC74E6954FA558B4aF836d95eCb91b;
+    address internal constant DSR_BALANCER_RATE_PROVIDER = 0x73750DbD85753074e452B2C27fB9e3B0E75Ff3B8;
+
+}
