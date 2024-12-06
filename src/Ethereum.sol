0894d151cab9cc50dcf49c4c32e6469b16b391a1
Lucas Manuel <lucasmanuel.tech@gmail.com>
2024-12-06 12:30:57 -0500

    feat: Add ALM multisigs (#24)

--- src/Ethereum.sol
+++ src/Ethereum.sol
@@ -59,6 +59,9 @@ library Ethereum {
     address internal constant ALM_PROXY       = 0x1601843c5E9bC251A3272907010AFa41Fa18347E;
     address internal constant ALM_RATE_LIMITS = 0x7A5FD5cf045e010e62147F065cEAe59e5344b188;
 
+    address internal constant ALM_FREEZER = 0x90D8c80C028B4C09C0d8dcAab9bbB057F0513431;
+    address internal constant ALM_RELAYER = 0x8a25A24EDE9482C4Fc0738F99611BE58F1c839AB;
+
     /******************************************************************************************************************/
     /*** Ethena Addresses                                                                                           ***/
     /******************************************************************************************************************/
