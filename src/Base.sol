399e26da866f3182fd40e67ad6cdc42fc7b1723f
Lucas Manuel <lucasmanuel.tech@gmail.com>
2024-12-20 10:41:57 -0500

    feat: add new addresses

--- src/Base.sol
+++ src/Base.sol
@@ -35,6 +35,9 @@ library Base {
     address internal constant ALM_PROXY       = 0x2917956eFF0B5eaF030abDB4EF4296DF775009cA;
     address internal constant ALM_RATE_LIMITS = 0x983eC82E45C61a42FDDA7B3c43B8C767004c8A74;
 
+    address internal constant ALM_FREEZER = 0x90D8c80C028B4C09C0d8dcAab9bbB057F0513431;
+    address internal constant ALM_RELAYER = 0x8a25A24EDE9482C4Fc0738F99611BE58F1c839AB;
+
     /******************************************************************************************************************/
     /*** Governance Relay Addresses                                                                                 ***/
     /******************************************************************************************************************/
