2077b3a1bcf5ce555118dcb5cdfc308c89e511f3
TheMj0ln1r <charan.nomula.cn@gmail.com>
2026-09-11 11:42:30 +0530

    feat: add spusdc pau addresses

--- src/XLayer.sol
+++ src/XLayer.sol
@@ -20,6 +20,27 @@ library XLayer {
     address internal constant SPARK_RECEIVER = 0x4bd50B9c00Ae19e8B59723F27645C7A5cCe7a4A0;  // xchain-helpers/OptimismReceiver.sol@1e362bf (v1.2.0)
 
     /******************************************************************************************************************/
+    /*** Spark PAU Core addresses                                                                                   ***/
+    /******************************************************************************************************************/
+
+    address internal constant BEACON                     = 0x5612697F3F8c393A860ac0C9cf5b315c248cB0a0;  // diamond-pau/Beacon.sol@cbf71b2 (v1.14.0)
+    address internal constant PAU_FACTORY                = 0x0B33b8974eDe2985ac9E41931314337C01543fFE;  // diamond-pau/PauFactory.sol@cbf71b2 (v1.14.0)
+    address internal constant ADMINISTERED_AGENT_FACTORY = 0x039bC8CAe7A5b2B981E5ED98B840C76c7FBacDAc;  // pau-administered-agent/AdministeredAgentFactory.sol@bfaaf70 (v1.0.0)
+    address internal constant DEFAULT_PAU_ASSEMBLER      = 0xaCae58f96C959A792FaeaEc72CA870c2E36B3C80;  // pau-assemblers/DefaultPauAssembler.sol@d7d6f08 (v1.0.0)
+
+    /******************************************************************************************************************/
+    /*** Spark PAU Facets addresses                                                                                 ***/
+    /******************************************************************************************************************/
+
+    address internal constant AAVE_FACET           = 0xc8C8930F28622D240fF380bC8b2224119b477249;  // diamond-pau/AAVEFacet.sol@cbf71b2 (v1.14.0)
+    address internal constant CCTP_FACET           = 0x4a966353B421dF6ddd08E432A1E55B884Cb92D4E;  // diamond-pau/CCTPFacet.sol@cbf71b2 (v1.14.0)
+    address internal constant ERC4626_FACET        = 0x3fC45da74c30644172620520F6bAf6783aec11F2;  // diamond-pau/ERC4626Facet.sol@cbf71b2 (v1.14.0)
+    address internal constant LAYER_ZERO_FACET     = 0x936D542661734F2D9a96A75fF27B1BC1D2B5dfB5;  // diamond-pau/LayerZeroFacet.sol@cbf71b2 (v1.14.0)
+    address internal constant SPARK_VAULT_FACET    = 0x3a35fF0B4D6375f8F3692562d378C1e5Ed482053;  // diamond-pau/SparkVaultFacet.sol@cbf71b2 (v1.14.0)
+    address internal constant TRANSFER_ASSET_FACET = 0xF0830c3B800a36ff3F3E9229e0443CD5b7779e0D;  // diamond-pau/TransferAssetFacet.sol@cbf71b2 (v1.14.0)
+    // address internal constant UNISWAP_V4_FACET     = // @TODO: Deploy facet
+
+    /******************************************************************************************************************/
     /*** Spark Liquidity Layer addresses                                                                            ***/
     /******************************************************************************************************************/
 
@@ -37,27 +58,25 @@ library XLayer {
     address internal constant SPARK_VAULT_V2_SPUSDT = 0xc358c90D32375721Cb3924320Fdc2F8B694347Ca;  // openzeppelin-contracts/ERC1967Proxy.sol@c64a1edb (v5.4.0)
 
     /******************************************************************************************************************/
+    /*** Spark SPUSDC Seggregated PAU addresses                                                                     ***/
+    /******************************************************************************************************************/
+
+    address internal constant SPUSDC_PAU_ACCESS_CONTROLS = 0x30271Ae5d90B34f0f8BAA840AaE63a7DBF347e84;
+    address internal constant SPUSDC_PAU_ALM_PROXY       = 0xe6D5d041Fc5e7fDD0A53C13e78a1cc7e4ffCb667;
+    address internal constant SPUSDC_PAU_CONTROLLER      = 0x8d19d306cA6075f74B26B150383379aedB0250f6;
+    address internal constant SPUSDC_PAU_RATELIMITS      = 0xBC3Ea763532aA0dec71449414b85ca66c6dD63e2;
+
+    address internal constant SPUSDC_PAU_ADMINISTERED_AGENT         = 0x79b4055Eda153f739B5EA63C9B647c1a095059f5;
+    // address internal constant SPUSDC_PAU_ADMINISTERED_AGENT_ADMIN   = ; // @TODO : add admin address
+    address internal constant SPUSDC_PAU_ADMINISTERED_AGENT_ACTOR   = 0x8a25A24EDE9482C4Fc0738F99611BE58F1c839AB;
+    address internal constant SPUSDC_PAU_ADMINISTERED_AGENT_GRANTOR = 0x4B61A0E48dd1e300f64090C60F414c1aC6CbC514;
+    address internal constant SPUSDC_PAU_ADMINISTERED_AGENT_REVOKER = 0x90D8c80C028B4C09C0d8dcAab9bbB057F0513431;
+
+    /******************************************************************************************************************/
     /*** Spark Intents addresses                                                                                    ***/
     /******************************************************************************************************************/
 
     address internal constant SPARK_SAVINGS_INTENTS = 0x5bCD2f30FA1Bf675d5d6E793DAD7DdD487D21865;  // spark-savings-intents/SavingsVaultIntents.sol@d9045fc (v1.0.0)
-
-    /******************************************************************************************************************/
-    /*** Spark PAU Core addresses                                                                                   ***/
-    /******************************************************************************************************************/
-
-    address internal constant BEACON                     = 0x5612697F3F8c393A860ac0C9cf5b315c248cB0a0;  // diamond-pau/Beacon.sol@cbf71b2 (v1.14.0)
-    address internal constant PAU_FACTORY                = 0x0B33b8974eDe2985ac9E41931314337C01543fFE;  // diamond-pau/PauFactory.sol@cbf71b2 (v1.14.0)
-    address internal constant ADMINISTERED_AGENT_FACTORY = 0x039bC8CAe7A5b2B981E5ED98B840C76c7FBacDAc;  // pau-administered-agent/AdministeredAgentFactory.sol@bfaaf70 (v1.0.0)
-    address internal constant DEFAULT_PAU_ASSEMBLER      = 0xaCae58f96C959A792FaeaEc72CA870c2E36B3C80;  // pau-assemblers/DefaultPauAssembler.sol@d7d6f08 (v1.0.0)
-
-    /******************************************************************************************************************/
-    /*** Spark PAU Facets addresses                                                                                 ***/
-    /******************************************************************************************************************/
-
-    address internal constant CCTP_FACET           = 0x4a966353B421dF6ddd08E432A1E55B884Cb92D4E;  // diamond-pau/CCTPFacet.sol@cbf71b2 (v1.14.0)
-    address internal constant SPARK_VAULT_FACET    = 0x3a35fF0B4D6375f8F3692562d378C1e5Ed482053;  // diamond-pau/SparkVaultFacet.sol@cbf71b2 (v1.14.0)
-    address internal constant TRANSFER_ASSET_FACET = 0xF0830c3B800a36ff3F3E9229e0443CD5b7779e0D;  // diamond-pau/TransferAssetFacet.sol@cbf71b2 (v1.14.0)
 
     /*******************************************************************************************************************
 
